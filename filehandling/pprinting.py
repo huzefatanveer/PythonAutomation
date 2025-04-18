50a2cc16536d2027cea953dded78c3a4440b3073
Huzefa Tanveer <huzefatanvirici@gmail.com>
2025-04-18 19:56:20 +0500

    filhandling project1

--- filehandling/pprinting.py
+++ filehandling/pprinting.py
@@ -7,3 +7,9 @@ pprint.pformat(cats)
 fileObj = open('myCats.py', 'w')
 fileObj.write('cats = ' + pprint.pformat(cats) + '\n')
 fileObj.close()
+
+import myCats
+
+print(myCats.cats)
+print(myCats.cats[0])
+print(myCats.cats[0]['name'])
